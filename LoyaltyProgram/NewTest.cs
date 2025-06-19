d2b39a6e5849ee22c116ed91c289ab22217861ab
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-06-19 18:54:48 +0300

    Test swap 6

--- LoyaltyProgram/NewTest.cs
+++ LoyaltyProgram/NewTest.cs
@@ -22,7 +22,7 @@ namespace LoyaltyProgram
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello NewTest2, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello NewTest out of staging, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
