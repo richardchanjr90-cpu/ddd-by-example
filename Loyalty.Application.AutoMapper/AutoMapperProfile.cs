a671baa05f73a8cc893e1b38f098265a80648db1
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-09-24 16:52:55 +0300

    Updated AutoMapper

--- Loyalty.Application.AutoMapper/AutoMapperProfile.cs
+++ Loyalty.Application.AutoMapper/AutoMapperProfile.cs
@@ -1,15 +1,29 @@
 ﻿using AutoMapper;
 using Loyalty.Application.ViewModels;
 using Loyalty.Application.ViewModels.Location;
+using Loyalty.Application.ViewModels.LoyaltyProductGroup;
+using Loyalty.Application.ViewModels.LoyaltyProgram;
+using Loyalty.Application.ViewModels.Product;
+using Loyalty.Application.ViewModels.ProductGroup;
 using Loyalty.Application.ViewModels.Purchase;
 using Loyalty.Application.ViewModels.Venue;
+using Loyalty.Application.ViewModels.Worker;
 using Loyalty.Domain.Handlers.Queries.Commands.Locations;
+using Loyalty.Domain.Handlers.Queries.Commands.LoyaltyProductGroup;
+using Loyalty.Domain.Handlers.Queries.Commands.LoyaltyPrograms;
+using Loyalty.Domain.Handlers.Queries.Commands.Products;
 using Loyalty.Domain.Handlers.Queries.Commands.Purchase;
 using Loyalty.Domain.Handlers.Queries.Commands.Venue;
 using Loyalty.Domain.Handlers.Queries.Commands.VenueDetails;
+using Loyalty.Domain.Handlers.Queries.Commands.Workers;
+using Loyalty.Domain.Handlers.Queries.QueryResults.LoyaltyProductGroup;
+using Loyalty.Domain.Handlers.Queries.QueryResults.LoyaltyProgram;
+using Loyalty.Domain.Handlers.Queries.QueryResults.Product;
+using Loyalty.Domain.Handlers.Queries.QueryResults.ProductGroup;
 using Loyalty.Domain.Handlers.Queries.QueryResults.Purchase;
 using Loyalty.Domain.Handlers.Queries.QueryResults.Venue;
 using Loyalty.Domain.Handlers.Queries.QueryResults.VenueDetails;
+using Loyalty.Domain.Handlers.Queries.QueryResults.Worker;
 
 namespace Loyalty.Application.AutoMapper
 {
@@ -47,9 +61,29 @@ namespace Loyalty.Application.AutoMapper
 
             
             CreateMap<PurchaseViewModel, CreatePurchaseCommand>();
-
             CreateMap<ActivePurchaseResult, ActivePurchaseViewModel>();
             CreateMap<GetActivePurchaseResult, ActivePurchasesViewModel>();
+
+            CreateMap<GetLoyaltyProgramByIdQueryResult, LoyaltyProgramViewModel>();
+            CreateMap<LoyaltyProgramViewModel, CreateLoyaltyProgramCommand>();
+            CreateMap<LoyaltyProgramViewModel, UpdateLoyaltyProgramCommand>();
+
+            CreateMap<GetProductByIdQueryResult, ProductViewModel>();
+            CreateMap<ProductViewModel, CreateProductCommand>();
+            CreateMap<ProductViewModel, UpdateProductCommand>();
+
+            CreateMap<GetLoyaltyProductGroupByIdQueryResult, LoyaltyProductGroupViewModel>();
+            CreateMap<LoyaltyProductGroupViewModel, CreateLoyaltyProductGroupCommand>();
+            CreateMap<LoyaltyProductGroupViewModel, UpdateLoyaltyProductGroupCommand>();
+
+            CreateMap<GetProductGroupByIdQueryResult, ProductGroupViewModel>();
+            CreateMap<ProductGroupViewModel, ProductGroupViewModel>();
+            CreateMap<ProductGroupViewModel, ProductGroupViewModel>();
+
+            CreateMap<GetWorkerByIdQueryResult, WorkerViewModel>();
+            CreateMap<CreateWorkerCommand, WorkerViewModel>();
+            CreateMap<UpdateWorkerCommand, WorkerViewModel>();
+
         }
     }
 }
