83c1392bbd68869790f128edb7d22d541950c6ad
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-09-24 17:24:15 +0300

    Updated VM

--- Loyalty.Application.ViewModels/Purchase/ActivePurchaseViewModel.cs
+++ Loyalty.Application.ViewModels/Purchase/ActivePurchaseViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace Loyalty.Application.ViewModels.Purchase
+﻿using Newtonsoft.Json;
+
+namespace Loyalty.Application.ViewModels.Purchase
 {
     public class ActivePurchaseViewModel
     {
+        [JsonProperty("id")]
         public long Id { get; set; }
 
+        [JsonProperty("value")]
         public decimal? Value { get; set; }
     }
 }
