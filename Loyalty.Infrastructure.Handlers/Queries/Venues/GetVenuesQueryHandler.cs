1acdf102de788aee7043516f5817f6e670271fd8
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-10-15 18:06:13 +0300

    Temp commit, revert it

--- Loyalty.Infrastructure.Handlers/Queries/Venues/GetVenuesQueryHandler.cs
+++ Loyalty.Infrastructure.Handlers/Queries/Venues/GetVenuesQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Loyalty.Common.Shared.Settings;
@@ -9,27 +11,40 @@ using Loyalty.Domain.Handlers.Queries.QueryResults.Venue;
 using Loyalty.Infrastructure.Handlers.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using SecurityDriven.TinyORM.Extensions;
+using DbContext = SecurityDriven.TinyORM.DbContext;
 
 namespace Loyalty.Infrastructure.Handlers.Queries.Venues
 {
-    public class GetVenuesQueryHandler : BaseHandler, IGetVenuesQueryHandler
+    public class GetVenuesQueryHandler: IGetVenuesQueryHandler
     {
-        public GetVenuesQueryHandler(ILoyaltyDbContext context, IOptions<DbSettings> settings)
-            : base(context)
+        private readonly DbContext context;
+
+        public GetVenuesQueryHandler(DbContext context, IOptions<DbSettings> settings)
+            //: base(context)
         {
+            this.context = context;
         }
 
         public async Task<GetVenuesQueryResult> Handle(GetVenuesQuery request, CancellationToken cancellationToken)
         {
-            var venues = await Context.Venues
-                .Include(x => x.Location)
-                .Include(x => x.Workers)
-                .Where(x => x.Workers.Select(y => y.WorkerId).Contains(request.UserId))
-                .ToListAsync(cancellationToken);
+            //var venues = await Context.Venues
+            //    .Include(x => x.Location)
+            //    .Include(x => x.Workers)
+            //    .Where(x => x.Workers.Select(y => y.WorkerId).Contains(request.UserId))
+            //    .ToListAsync(cancellationToken);
+            var query = "SELECT v.Id FROM loyalty.Venue v " +
+                        "JOIN loyalty.Worker w ON v.Id = w.VenueId " +
+                        "JOIN loyalty.Location l ON l.VenueId = v.Id " +
+                        "WHERE v.IsArchived = 0 AND w.WorkerId = @workerId";
+
+            var parameters = new Dictionary<string, (object, Type)>();
+            parameters.Add("@workerId", request.UserId.WithType());
+            var rows = (await context.QueryAsync(query, parameters), cancellationToken);
 
             return new GetVenuesQueryResult
             {
-                Venues = venues?.ToResults()
+                Venues = null 
             };
         }
     }
--- Loyalty.Infrastructure.IoC/DI/LoyaltyDbExtensions.cs
+++ Loyalty.Infrastructure.IoC/DI/LoyaltyDbExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using Loyalty.Common.Shared.Settings;
 using Loyalty.Core.Contracts;
 using Loyalty.Infrastructure.DataAccess;
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using SecurityDriven.TinyORM;
 
 namespace Loyalty.Infrastructure.IoC.DI
 {
@@ -16,16 +18,16 @@ namespace Loyalty.Infrastructure.IoC.DI
 
         public static void SetupDb(this IServiceCollection services, IConfigurationRoot config)
         {
-            services.AddScoped<ILoyaltyDbContext, LoyaltyDbContext>();
-
+            //services.AddScoped<ILoyaltyDbContext, LoyaltyDbContext>();
             var connectionString = config[$"{nameof(DbSettings)}:{nameof(DbSettings.ConnectionString)}"];
 
-            //todo: remove for staging and prod;
-            services.AddEntityFrameworkSqlServer()
-                .AddLogging()
-                .AddDbContext<LoyaltyDbContext>(
-                    options => options.UseSqlServer(
-                        connectionString).UseLoggerFactory(MyLoggerFactory));
+            services.AddScoped<DbContext>((sp) => DbContext.Create(connectionString));
+            ////todo: remove for staging and prod;
+            //services.AddEntityFrameworkSqlServer()
+            //    .AddLogging()
+            //    .AddDbContext<LoyaltyDbContext>(
+            //        options => options.UseSqlServer(
+            //            connectionString).UseLoggerFactory(MyLoggerFactory));
         }
     }
 }
