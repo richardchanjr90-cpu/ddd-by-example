6342b107858fd6d79fec143b1dd19903ff6a4773
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-11-01 10:20:53 +0300

    Fixed 500 error when container does not exist

--- LoyaltyProgram/Http/VenueImages/VenueGetImagesFunction.cs
+++ LoyaltyProgram/Http/VenueImages/VenueGetImagesFunction.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,11 +24,15 @@ namespace LoyaltyProgram.Http.VenueImages
             [Blob("venue-images-{id}", FileAccess.Read)] CloudBlobContainer container)
         {
             log.LogInformation($"{nameof(VenueGetImagesFunction)} was triggered.");
+            var results = new List<string>();
+            var exists = await container.ExistsAsync();
 
-            var operation =
-                await container.ListBlobsSegmentedAsync(req.Query["prefix"], true, BlobListingDetails.None, 50, null, null, null);
-
-            var results = operation.Results.Select(item => item.Uri.ToString()).ToList();
+            if (exists)
+            {
+                var operation =
+                    await container.ListBlobsSegmentedAsync(req.Query["prefix"], true, BlobListingDetails.None, 50, null, null, null);
+                results = operation.Results.Select(item => item.Uri.ToString()).ToList();
+            }
 
             return await ExceptionWrapper.Handle(async () =>
             {
