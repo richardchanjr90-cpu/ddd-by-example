93b2344e6dfd0aff9c60e5a87dc61f389b4e437e
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-11-04 06:10:55 +0300

    Updated integrations

--- Loyalty.Domain.Handlers.Notifications/LoyaltyPrograms/CreateLoyaltyProgramNotification.cs
+++ Loyalty.Domain.Handlers.Notifications/LoyaltyPrograms/CreateLoyaltyProgramNotification.cs
@@ -7,6 +7,8 @@ namespace Loyalty.Domain.Handlers.Notifications.LoyaltyPrograms
     {
         public long Id { get; set; }
 
+        public long VenueId { get; set; }
+
         public string Name { get; set; }
 
         public DateTime StartDate { get; set; }
