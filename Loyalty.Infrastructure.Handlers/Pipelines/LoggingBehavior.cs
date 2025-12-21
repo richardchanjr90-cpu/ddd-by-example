967fa46d2ee778210786527938f3c875d7ecc25d
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-12-21 13:17:45 +0300

    Fixed issues

--- Loyalty.Infrastructure.Handlers/Pipelines/LoggingBehavior.cs
+++ Loyalty.Infrastructure.Handlers/Pipelines/LoggingBehavior.cs
@@ -19,11 +19,10 @@ public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
         RequestHandlerDelegate<TResponse> next)
     {
         var log = logFactory.CreateLogger(typeof(TRequest).Name);
-        //var serializedRequest = JsonConvert.SerializeObject(request);
-        //log.LogInformation($"Handling {typeof(TResponse).Name}: {serializedRequest}", serializedRequest);
+        var serializedRequest = JsonConvert.SerializeObject(request);
+        log.LogInformation($"Handling {typeof(TResponse).Name}: {serializedRequest}");
         var response = await next();
         log.LogInformation($"Handled {typeof(TResponse).Name}");
-
         return response;
     }
 }
