b70c376a024195d0a246e11922c92765ed872395
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2025-12-30 14:07:40 +0300

    Added loader.io function

---
+++ src/LoyaltyProgram/Http/Ping/LoaderIoFunction.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LoyaltyProgram.Http.Ping
+{
+    public class LoaderIoFunction
+    {
+        [FunctionName("LoaderIoFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "loaderio-05c951e4f59a15bcb4003500bfa99a94")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($"{nameof(LoaderIoFunction)} was triggered.");
+            var bytes = Encoding.UTF8.GetBytes("loaderio-05c951e4f59a15bcb4003500bfa99a94");
+            return new FileContentResult(bytes, "text/plain");
+        }
+    }
+}
