eca9acec7b9339e0ebabbc413999176c4d2e775b
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2026-01-16 18:12:10 +0300

    Updated logging

--- src/Loyalty.Infrastructure.Handlers/Pipelines/LoggingBehavior.cs
+++ src/Loyalty.Infrastructure.Handlers/Pipelines/LoggingBehavior.cs
@@ -24,12 +24,12 @@ namespace Loyalty.Infrastructure.Handlers.Pipelines
             try
             {
                 var serializedRequest = JsonConvert.SerializeObject(request);
-                var log = logFactory.CreateLogger(typeof(TRequest).Name);
-                log.LogInformation($"Handling {typeof(TResponse).Name}: {serializedRequest}");
+                var log = logFactory.CreateLogger(typeof(TRequest)?.Name);
+                log.LogInformation($"Handling {typeof(TResponse)?.Name}: {serializedRequest}");
 
                 var response = await next();
 
-                log.LogInformation($"Handled {typeof(TResponse).Name}");
+                log.LogInformation($"Handled {typeof(TResponse)?.Name}");
 
                 return response;
             }
