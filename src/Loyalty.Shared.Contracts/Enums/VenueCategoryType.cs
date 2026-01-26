a02a8ac49c623be53551aada4af2acb78e35694e
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2026-01-26 16:49:28 +0300

    Updated values

--- src/Loyalty.Shared.Contracts/Enums/VenueCategoryType.cs
+++ src/Loyalty.Shared.Contracts/Enums/VenueCategoryType.cs
@@ -3,7 +3,7 @@
 namespace Loyalty.Shared.Contracts.Enums
 {
     [Flags]
-    public enum VenueCategoryType : ulong
+    public enum VenueCategoryType: long
     {
         Cafe = 1,
         CoffeeShop = 2,
@@ -21,16 +21,16 @@ namespace Loyalty.Shared.Contracts.Enums
         StreetFood = 8192,
         ArtBar = 16384,
         Pizzeria = 32768,
-        Pub = 65536‬,
+        Pub = 65536,
         Shawarma = 131072,
-        Gastrobar = 262144‬,
+        Gastrobar = 131072,
         Bakery = 524288,
         Burgers = 1048576,
-        Sushi = 2097152,
-        CoffeePoint = 4194304‬,
+        Sushi =2097152,
+        CoffeePoint = 4194304,
         FoodTruck = 8388608,
-        Grillbar = 16777216‬,
-        Coworking = 33554432‬,
-        Pancake = 67108864‬,
+        Grillbar = 16777216,
+        Coworking = 33554432,
+        Pancake = 67108864
     }
 }
