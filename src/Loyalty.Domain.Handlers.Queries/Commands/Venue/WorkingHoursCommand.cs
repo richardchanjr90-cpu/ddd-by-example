6c625a9c793f13ca52dd2e293b0ad3d863e3183a
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2026-04-07 11:52:42 +0300

    Updated

--- src/Loyalty.Domain.Handlers.Queries/Commands/Venue/WorkingHoursCommand.cs
+++ src/Loyalty.Domain.Handlers.Queries/Commands/Venue/WorkingHoursCommand.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Loyalty.Domain.Handlers.Queries.Commands.Venue
 {
     public class WorkingHoursCommand
     {
+        [JsonPropertyName("day")]
         public DayOfWeek Day { get; set; }
 
+        [JsonPropertyName("from")]
         public int? From { get; set; }
 
+        [JsonPropertyName("to")]
         public int? To { get; set; }
     }
 }
