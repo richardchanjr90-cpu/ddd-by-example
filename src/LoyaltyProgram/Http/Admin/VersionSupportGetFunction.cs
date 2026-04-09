0740bb52095330af1f90f2d4ca82a36c69c4f8c2
Richard Chan <293101470+richardchanjr90-cpu@users.noreply.github.com>
2026-04-09 06:26:34 +0300

    Added versioning

---
+++ src/LoyaltyProgram/Http/Admin/VersionSupportGetFunction.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LoyaltyProgram.Http.Admin
+{
+    public class VersionSupportGetFunction
+    {
+        [FunctionName("VersionSupportGetFunction")]
+        public IActionResult Run(
+            int major,
+            int minor,
+            int build,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "control/admins/version/{major}/{minor}/{build}")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($"{nameof(VersionSupportGetFunction)} was triggered.");
+
+            return new OkObjectResult(true);
+        }
+    }
+}
